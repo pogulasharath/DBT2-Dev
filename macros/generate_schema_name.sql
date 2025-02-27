0b8c2fffff657e040903cdd98368128b495a25da
sharathp109@gmail.com <sharathp109@gmail.com>
2025-02-27 09:50:51 +0000

    commit3

---
+++ macros/generate_schema_name.sql
@@ -0,0 +1,16 @@
+/* Add file generate_schema_name.sql at the macro folder to generate the custom schema from DBT */
+
+{% macro generate_schema_name(custom_schema_name, node) -%}
+
+    {%- set default_schema = target.schema -%}
+    {%- if custom_schema_name is none -%}
+
+        {{ default_schema }}
+
+    {%- else -%}
+
+        {{ custom_schema_name | trim }}
+
+    {%- endif -%}
+
+{%- endmacro %}
--- models/bronze-dc-sales/patient1.sql
+++ models/bronze-dc-sales/patient1.sql
@@ -1,3 +1,4 @@
+{{ config(materialized='table') }}
 with src_data as (select * from dc_hc_sales_dev.bronze_dc_sales.patient)
 select *
 from src_data
